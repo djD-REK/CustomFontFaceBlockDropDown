e7517792f9d1d1133f27f28bb8fdb20cdcac2aeb
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-14 22:42:28 -0400

    Added tests and published Block

--- __test__/AMP.spec.js
+++ __test__/AMP.spec.js
@@ -1,35 +1,74 @@
-import React from "react";
-import { mount } from "enzyme";
+import React from "react"
+import { mount } from "enzyme"
 import {
   mockUtils as utils,
   joinClasses,
-} from "@volusion/element-block-utils/test-utils";
-import { block as Block, defaultConfig } from "../src";
+} from "@volusion/element-block-utils/test-utils"
+import { block as Block, defaultConfig } from "../src"
 
-let props;
-describe("The Block when loaded over AMP", () => {
+import { flushToStyleTag } from "aphrodite"
+
+let props
+describe("The Block", () => {
   beforeEach(() => {
     props = {
       data: {},
       utils: { ...utils, isAmpRequest: true },
       joinClasses,
       queryParams: {},
-    };
-  });
+    }
+  })
   it("renders without errors", () => {
-    mount(<Block {...props} />);
-  });
+    mount(<Block {...props} />)
+  })
   describe("when there is no custom data", () => {
     it("should render the block with the default content", () => {
-      const wrapper = mount(<Block {...props} />);
-      expect(wrapper.text()).toBe(defaultConfig.text);
-    });
-  });
+      const wrapper = mount(<Block {...props} />)
+      expect(wrapper.text()).toBe(defaultConfig.text)
+    })
+    it("should render the block with the default font sizes", () => {
+      const wrapper = mount(<Block {...props} />)
+      // Force Aphrodite to output the CSS-in-JS styles in time to test them:
+      flushToStyleTag()
+      const h1Element = wrapper.find("h1").at(0)
+      // Make sure our CSS-in-JS decorator successfully injected the CSS class
+      expect(h1Element.hasClass(/example_(\w+)/)).toBe(true)
+      // Expect the computed CSS style to match the default heading font sizes
+      const h1Styles = getComputedStyle(h1Element.getDOMNode())
+      expect(h1Styles.getPropertyValue("font-family")).toBe(
+        defaultConfig.fontFamily
+      )
+      expect(h1Styles.getPropertyValue("font-weight")).toBe(
+        defaultConfig.fontWeight
+      )
+    })
+  })
   describe("when given custom data", () => {
     it("should render the block using the custom data", () => {
-      const customText = "Custom Block Text";
-      const wrapper = mount(<Block {...props} text={customText} />);
-      expect(wrapper.text()).toBe(customText);
-    });
-  });
-});
+      const customText = "Custom Block Text"
+      const wrapper = mount(<Block {...props} text={customText} />)
+      expect(wrapper.text()).toBe(customText)
+    })
+    it("should render the block with the custom font sizes", () => {
+      const customFontFamily = "'Dancing Script', cursive"
+      const customFontWeight = "normal"
+
+      const wrapper = mount(
+        <Block
+          {...props}
+          fontFamily={customFontFamily}
+          fontWeight={customFontWeight}
+        />
+      )
+      // Force Aphrodite to output the CSS-in-JS styles in time to test them:
+      flushToStyleTag()
+      const h1Element = wrapper.find("h1").at(0)
+      // Make sure our CSS-in-JS decorator successfully injected the CSS class
+      expect(h1Element.hasClass(/example_(\w+)/)).toBe(true)
+      // Expect the computed CSS style to match the default heading font sizes
+      const h1Styles = getComputedStyle(h1Element.getDOMNode())
+      expect(h1Styles.getPropertyValue("font-family")).toBe(customFontFamily)
+      expect(h1Styles.getPropertyValue("font-weight")).toBe(customFontWeight)
+    })
+  })
+})
