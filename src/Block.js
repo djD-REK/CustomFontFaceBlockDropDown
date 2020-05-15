8b11fea246d38a034699cb777ef62be3b8f9bb5d
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-14 22:05:54 -0400

    prettier

--- src/Block.js
+++ src/Block.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { css, StyleSheet } from 'aphrodite/no-important';
-import { getStyles } from './getStyles';
-import { defaultConfig } from './configs';
+import React from "react";
+import { css, StyleSheet } from "aphrodite/no-important";
+import { getStyles } from "./getStyles";
+import { defaultConfig } from "./configs";
 
-const Block = props => {
-    const classes = StyleSheet.create(getStyles(props));
-    return <h1 className={css(classes.example)}>{props.text}</h1>;
+const Block = (props) => {
+  const classes = StyleSheet.create(getStyles(props));
+  return <h1 className={css(classes.example)}>{props.text}</h1>;
 };
 
 Block.defaultProps = defaultConfig;
--- src/configs.js
+++ src/configs.js
@@ -1,12 +1,16 @@
-import { ElementPropTypes } from '@volusion/element-proptypes';
+import { ElementPropTypes } from "@volusion/element-proptypes";
 
 export const configSchema = {
-    text: {
-        label: 'Text content',
-        type: ElementPropTypes.string
-    }
+  text: {
+    label: "Text content",
+    type: ElementPropTypes.string,
+  },
+  fontFamily: {
+    label: "Dropdown",
+    type: ElementPropTypes.oneOf(["News", "Photos"]),
+  },
 };
 
 export const defaultConfig = {
-    text: 'Element Starter Block'
+  text: "Element Starter Block",
 };
