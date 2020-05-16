10031c69c0790561e66c3e9cbb892731950b80eb
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-15 20:37:27 -0400

    Remove useEffect

--- src/Block.js
+++ src/Block.js
@@ -4,14 +4,6 @@ import { getStyles } from "./getStyles"
 import { defaultConfig } from "./configs"
 
 const Block = (props) => {
-  // React's Effect Hook (useEffect) runs a function when the Block is rendered
-  useEffect(() =>
-    // Add a link to the font stylesheet
-    {
-      // The addLink utility is a helper function to add a stylesheet to the <head> element
-    }, [])
-  // The second parameter is an empty array, [], to make useEffect run just once
-
   const classes = StyleSheet.create(getStyles(props))
   return <h1 className={css(classes.example)}>{props.text}</h1>
 }
